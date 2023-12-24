af43cf3a3621813c2f4871f934c2f998abfed495
MrMorales <mrphysics3.14@gmail.com>
2023-12-24 12:45:31 -0500

    Server added

--- public/index.js
+++ public/index.js
@@ -26,18 +26,12 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.get("/", async (req, res) => {
-  console.log(req.body);
+// app.get("/PP-Gallery", async (req, res) => {
+    
+//   console.log(req.body);
 
-  // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
-  // Render the index.ejs file with a single *random* activity that comes back
-  // from the API request.
-  // Step 3: If you get a 404 error (resource not found) from the API request.
-  // Pass an error to the index.ejs to tell the user:
-  // "No activities that match your criteria."
-});
+
+// });
 
 app.listen(port, () => {
   log(`Serveris up and running `);
